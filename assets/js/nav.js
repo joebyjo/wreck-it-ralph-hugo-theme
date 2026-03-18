5808dd260c9033611e0124907008c344661be8bc
joebyjo <joebyjo@gmail.com>
2026-03-18 15:15:34 +1030

    - updated css - added better navbar for mobile

---
+++ assets/js/nav.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.nav-toggle');
+const menu = document.querySelector('.main-menu');
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
